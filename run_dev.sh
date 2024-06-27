4edb1cf5cf2aeb94759e8f1138bb5a0b2caa3648
Maximilian Dewald <maximilian.dewald@envelio.de>
2024-06-27 14:38:39 +0200

    beautify output
    simplify dev script
    
    simplify dev script

--- run_dev.sh
+++ run_dev.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# Colors
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+NC='\033[0m'         # No color
+
+# Function to run a command with stdbuf and prefix its output
+run_with_prefix() {
+    local label=$1
+    shift 1
+    stdbuf -oL "$@" 2>&1 | while IFS= read -r line; do
+        echo -e "${label}${YELLOW} | ${NC}${line}"
+    done &
+}
+
 # Start Vite development server (frontend)
-FORCE_COLOR=1 npm run --prefix ./frontend dev &
+FORCE_COLOR=1 run_with_prefix "${BLUE}frontend" npm run --prefix ./frontend dev
 
 # Start uvicorn server (backend)
-uvicorn backend.src.main:app --host 0.0.0.0 --port ${BACKEND_PORT} --reload --reload-dir "/app/backend/src" &
+PYTHONUNBUFFERED=1 run_with_prefix "${GREEN}backend " uvicorn backend.src.main:app --host 0.0.0.0 --port "${BACKEND_PORT}" --reload --use-colors --reload-dir ./backend/src
 
 # Wait for any process to exit
 wait -n
