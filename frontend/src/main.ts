3833b0b3456143f148dc6fb6e3c6a40854cee68e
Maximilian Dewald <maximilian.dewald@envelio.de>
2024-06-27 14:39:38 +0200

    add tailwind

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,7 +6,6 @@ import App from '@/App.vue'
 import 'primevue/resources/themes/aura-light-teal/theme.css';
 import '@/assets/main.css';
 
-
 const app = createApp(App);
 app.use(PrimeVue);
 app.mount('#app');
